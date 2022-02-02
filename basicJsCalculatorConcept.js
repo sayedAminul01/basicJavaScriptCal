95a9e172b35296dacae132a2c412675d384981da
sayedEminul <89654789+sayedEminul@users.noreply.github.com>
2022-02-02 16:19:55 +0530

    Add files via upload

---
+++ basicJsCalculatorConcept.js
@@ -0,0 +1,22 @@
+console.log("This is a Basic JavaScript Calculator till I know.")
+// Here we declare 2 variable and store 2 integer value with variables name num1 num2.
+var num1 = 7;
+var num2 = 6;
+// Here add of num1 and num2.
+console.log("The addition of num1 and num2 is:", num1 + num2);
+//Here subtract of num1 and num2
+console.log("The subtraction of num1 and num2 is:",num1 - num2);
+// Here multiply of num1 and num2.
+console.log("The multiply of num1 and num2 is:", num1 * num2);
+// Here divide of num1 and num2
+console.log("The division of num1 and num2 is:", num1 / num2);
+// Here remainder of num1 and num2 
+console.log("The remainder of num1 and num2 is:", num1 % num2);
+// Here Power of num1 and num2
+console.log("The power of num1 and num2 is:", num1 ** num2);
+
+
+
+
+
+
